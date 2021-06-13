513beb4fdc0c2c1072c82f9cfdcb69e0bfed4a15
Chinmay Tewari <myemail@email.com>
2021-06-13 15:59:25 +0530

    second commit

--- app.js
+++ app.js
@@ -200,6 +200,6 @@ app.post("/", function(req, res) {
 });
 
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("The server is running on Port 3000");
 });
